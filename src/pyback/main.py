a93d7e7c2bc8bd2ab55a71d1365deeb5d26c9440
Vladimir Klepov <vklepov@ozon.ru>
2023-09-24 20:44:31 +0300

    Add docstrings

--- src/pyback/main.py
+++ src/pyback/main.py
@@ -1,3 +1,4 @@
+"""FastAPI router module."""
 from fastapi import FastAPI, HTTPException
 
 from .models import Greeting, GreetingBuilder
@@ -8,18 +9,30 @@ storage = {}
 
 @app.get("/")
 async def hello(receiver: str = "World") -> Greeting:
+    """Greet someone.
+
+    - **receiver**: Who to greet.
+    """
     return Greeting.from_receiver(receiver)
 
 
 @app.post("/greetings/")
 async def create_greeting(body: GreetingBuilder) -> Greeting:
+    """Save a greeting for later use.
+
+    - **receiver**: Who to greet.
+    """
     greeting = Greeting.from_builder(body)
     storage[greeting.id] = greeting
     return greeting
 
 
 @app.get("/greetings/{id}")
-async def get_greeting(id) -> Greeting:
+async def get_greeting(id: str) -> Greeting:
+    """Get a previously saved greeting.
+
+    - **id**: ID of the saved greeting from POST /greetings.
+    """
     if id not in storage:
         raise HTTPException(status_code=404, detail=f"Greetgng {id} not found")
     return storage[id]
--- src/pyback/models.py
+++ src/pyback/models.py
@@ -1,20 +1,28 @@
+"""Models for the hello app."""
+
 from uuid import uuid4
 
 from pydantic import BaseModel
 
 
 class GreetingBuilder(BaseModel):
+    """The data to build a greeting."""
+
     receiver: str
 
 
 class Greeting(BaseModel):
+    """A hello for someone."""
+
     message: str
     id: str | None = None
 
     @staticmethod
     def from_builder(builder: GreetingBuilder):
+        """Create greeting with random id from the data specified."""
         return Greeting(message=f"Hello, {builder.receiver}", id=str(uuid4()))
 
     @staticmethod
     def from_receiver(receiver: str):
+        """Create anonymous greeting for the receiver."""
         return Greeting(message=f"Hello, {receiver}")
